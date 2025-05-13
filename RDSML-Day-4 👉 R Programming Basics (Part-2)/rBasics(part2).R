427748aeb944b320e3ab98627595cce9e5c356ee
PriyataRouth <priyata.23180102@bau.edu.bd>
2025-05-13 13:39:03 +0600

    Day-2 basics part

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBasics(part2).R
@@ -0,0 +1,45 @@
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = 'priyata'
+class(name)
+print(name)
+
+c = "52"
+# print(c+ 9)
+print(a +4)
+print(a +7)
+a > b
+class(a > b)
+class(a < b)
+print(a < b)
+x = 19
+y = 22-3
+print(x == y)
+print(x != y)
+print(y > x)
+print(y >= x)
+# logical operator in R
+# & logical operator
+4 == 4 && 5 == 5
+# || or logical operator
+y = 19
+x != y || x == Y
+x =19
+y = 19
+x != 19 || x == y
+
+# simple Mathematical operators in R
+
+z = 6 + 9 - 8 * (2 / 3)
+print(z)
+print(z)
+w =( 4 + (3 - 3)) * 2 / 3
+print(w)
+y = 4^4
+print(y)
+11 / 5
+11 %% 5
+11 %/% 5
