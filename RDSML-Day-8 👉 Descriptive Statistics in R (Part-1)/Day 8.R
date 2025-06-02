ee948d588a22197fef5f2b84bf6ba9ac381e159a
PriyataRouth <priyata.23180102@bau.edu.bd>
2025-06-02 21:36:28 +0600

    Day 8

---
+++ RDSML-Day-8 👉 Descriptive Statistics in R (Part-1)/Day 8.R
@@ -0,0 +1,13 @@
+#Descriptive Statistics using the psych package
+
+install.packages("psych")
+library(psych)  #to load package
+
+data()
+# some data sets in R
+AirPassengers
+BOD
+Formaldehyde   
+
+#using the New York airport flight data
+install.packages("nycflights13")
