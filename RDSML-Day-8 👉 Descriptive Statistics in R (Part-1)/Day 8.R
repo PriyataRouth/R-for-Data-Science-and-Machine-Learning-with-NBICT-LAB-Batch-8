78024e252c5fe15bdcc18635f4d55926e23965de
PriyataRouth <priyata.23180102@bau.edu.bd>
2025-06-03 00:27:54 +0600

    day 8

--- RDSML-Day-8 👉 Descriptive Statistics in R (Part-1)/Day 8.R
+++ RDSML-Day-8 👉 Descriptive Statistics in R (Part-1)/Day 8.R
@@ -2,8 +2,8 @@
 
 install.packages("psych")
 library(psych)  #to load package
-
 data()
+
 # some data sets in R
 AirPassengers
 BOD
@@ -11,3 +11,12 @@ Formaldehyde
 
 #using the New York airport flight data
 install.packages("nycflights13")
+attach(nycflights13::flights)
+
+#Lets look at the first 6 records using head () function
+head(nycflights13::flights)
+summary(distance)
+describe(distance)
+demo = cbind(arr_delay, dep_delay, distance)
+demo
+describe(demo)
