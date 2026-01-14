1fe34133236d637a56690f2361841729544c0c05
PriyataRouth <priyata.23180102@bau.edu.bd>
2026-01-14 23:25:51 +0600

    Update binomial_distribution.R

--- binomial_distribution.R
+++ binomial_distribution.R
@@ -33,3 +33,25 @@ pbinom(q = 5, size = 10, prob = 0.5)
 
 # You toss a coin 5 times, what is the probabilityof getting exactly 3 heads?
 pbinom(q = 3, size = 5,prob = 0.5) - pbinom(q = 2, size = 5,prob = 0.5)
+
+# Explaining q binom
+# what is the smalest number of successes x in 10 trials (with 50% success chance) such that the probability of geting x or fewer successes is at least 30%?
+qbinom(p = 0.30, size = 10, prob = 0.5)
+# qbinom is revers of pbinom
+
+# p = 0.3 means cumulative probability 30%
+# size : 10 means number of trials (e.g: 10 coin tosses)
+# prob: 0.5 = probability of success
+
+
+
+# Explaining dbiunom
+# dbinom clculating the probability of exact number of success
+
+dbinom(x = 3, size = 5, prob = 0.5)
+dbinom(x = 3, size = 10, prob = 0.25)
+
+#dbinom(x , size, prob) 
+# x-3: number of successes you want
+# size -5: number of independent trials
+# prob-0.5 :probability of successes in each trials
