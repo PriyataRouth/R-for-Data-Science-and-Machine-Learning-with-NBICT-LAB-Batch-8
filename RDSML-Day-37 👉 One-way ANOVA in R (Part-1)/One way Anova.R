ebb69b7da663739c85aea18d7516fef2658d458f
PriyataRouth <priyata.23180102@bau.edu.bd>
2026-01-22 00:19:03 +0600

    Day- 37

---
+++ RDSML-Day-37 👉 One-way ANOVA in R (Part-1)/One way Anova.R
@@ -0,0 +1,26 @@
+#one-way Anove
+# importing the dataset
+PlantGrowth <- PlantGrowth
+
+# importing the dplyr library
+install.packages("dplyr")
+library(dplyr)
+
+# computing the group means, sd, & se
+stats <- PlantGrowth %>%
+  group_by(group) %>% 
+  summarise(
+    mean = mean(weight), sd = sd(weight),
+    se = sd(weight) / sqrt(n())
+)  
+
+print(stats)
+
+#creating groupwise box plots
+
+boxplot(weight ~ group, 
+        data = PlantGrowth,
+        main = "PlantGrowth data",
+        ylab = "Dried weight of plants", 
+        col = "lightgray"
+        )
