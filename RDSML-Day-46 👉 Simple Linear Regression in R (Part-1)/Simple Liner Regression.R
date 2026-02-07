2abf8557b4ac5de2cb71199e36db88be9edd2875
PriyataRouth <priyata.23180102@bau.edu.bd>
2026-02-07 22:52:26 +0600

    Update.R

--- RDSML-Day-46 👉 Simple Linear Regression in R (Part-1)/Simple Liner Regression.R
+++ RDSML-Day-46 👉 Simple Linear Regression in R (Part-1)/Simple Liner Regression.R
@@ -14,4 +14,34 @@ test_set <- subset(dataset, split == FALSE)
 
 # Fitting the Simple Linear Regression model to the training set
 regressor <- lm(formula = Salary ~ YearsExperience, 
-                data = training_set.
+                data = training_set)
+
+# Checking the summary of the model
+summary(regressor)
+
+# Predicting the test set results
+y_pred = predict(regressor, newdata = training_set)
+
+# Visualizing the Training set results
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x=training_set$YearsExperience, y=training_set$Salary),
+             colour='red') +
+  geom_line(aes(x=training_set$YearsExperience, y=y_pred),
+            colour='blue') +
+  ggtitle("Salary vs Experience") +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+
+# Visualizing the Test set results
+
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x=test_set$YearsExperience, y=test_set$Salary),
+             colour='red') +
+  geom_line(aes(x=test_set$YearsExperience, y=predict(regressor, newdata = test_set)),
+            colour='blue') +
+  ggtitle("Salary vs Experience (Test Set)") +
+  xlab('Years of experience') +
+  ylab('Salary')
