8da583c4f91ece01becd9d1131f1cbd9e20d5f5b
PriyataRouth <priyata.23180102@bau.edu.bd>
2026-02-14 21:38:38 +0600

    Update svr.R

--- RDSML-Day-53 👉 Support Vector Regression in R (Part-1)/svr.R
+++ RDSML-Day-53 👉 Support Vector Regression in R (Part-1)/svr.R
@@ -36,3 +36,48 @@ indices <- seq_len(n)
 train_idx <- sample(indices, size = 0.7 * n)
 train_set <- bike[train_idx, ]
 test_set <- bike[-train_idx, ]
+
+# Scale numeric features 
+
+# Identify numeric predictors
+num_cols <- c("temp", "atemp", "hum", "windspeed")
+
+# Compute scaling parameters on training data
+train_means <- sapply(train_set[, num_cols], mean)
+train_sds <- sapply(train_set[, num_cols], sd)
+
+# Scale Function
+scale_num <- function(df){
+  df[, num_cols] <- sweep(df[, num_cols], 2, train_means, "-")
+  df[, num_cols] <- sweep(df[, num_cols], 2, train_sds, "/")
+  df
+}
+
+train_scaled <- scale_num(train_set)
+test_scaled <- scale_num(test_set)
+
+# Building a linear regression model
+lm_model <- lm(cnt ~ ., data = train_scaled)
+summary(lm_model)
+
+lm_pred <- predict(lm_model, newdata = test_scaled)
+
+# Evaluation metrics for linear regression
+
+# Calculating root mean squared error
+rmse <- function(actual, pred) sqrt(mean((actual-pred)^2))
+lm_rmse <- rmse(test_scaled$cnt, lm_pred)
+lm_rmse
+
+# Calculating mean absolute error
+mae <- function(actual, pred) mean(abs(actual-pred))
+lm_mae <- mae(test_scaled$cnt, lm_pred)
+lm_mae
+
+# Building svr model
+
+svr <- svm(cnt ~ ., data = train_scaled, kernel = "linear")
+svr_pred <- predict(svr, newdata = test_scaled)
+
+svr_rmse <- rmse(test_scaled$cnt, svr_pred)
+svr_mae <- mae(test_scaled$cnt, svr_pred)
